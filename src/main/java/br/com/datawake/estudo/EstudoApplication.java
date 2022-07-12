2897dd072c98c2a43227d5cfdfbc0433c183ad02
Amanda Ramos de Siqueira <aramos@paranoa.com.br>
2022-07-12 15:26:44 -0300

    Testes

--- src/main/java/br/com/datawake/estudo/EstudoApplication.java
+++ src/main/java/br/com/datawake/estudo/EstudoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstudoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EstudoApplication.class, args);
+
+		//SpringApplication.run(EstudoApplication.class, args);
+
+		System.out.println("Hello Word");
+
 	}
 
 }
